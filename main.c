fbe170e48d63341f8b5eab0ad6792a47ee36023f
Rodolfo Viola <rodolfoviolac@gmail.com>
2018-03-15 20:20:35 -0300

    Added git ignore

--- main.c
+++ main.c
@@ -2,14 +2,14 @@
 #import "lex.yy.c"
 
 int main() {
-  FILE* testFile = fopen("/Users/leonardodalcin/CLionProjects/Compiladores/testFile.txt", "r");
+    //FILE* testFile = fopen("/Users/leonardodalcin/CLionProjects/Compiladores/testFile.txt", "r");
+    FILE* testFile = fopen("/Users/Rodolfo/Documents/Mega/UFRGS/Compiladores/compiler/testFile.txt", "r");
   int token;
   yyin = testFile;
   while( (token = yylex()) ) {
     printf("at line  %d \n", getLineNumber());
 
   }
-
   return 0;
 }
 
