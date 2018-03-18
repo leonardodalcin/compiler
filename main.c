05ebcaabef46631592dee5f7e7efdee62d1de845
Leonardo Dalcin <leonardodalcin@outlook.com>
2018-03-18 15:46:05 -0300

    Make.

--- main.c
+++ main.c
@@ -1,23 +1,4 @@
-/*  ################      Trabalho Prático - Compiladores [PARTE 1]  ################    */
-/*                      Rodolfo Viola Carvalho - 265043   (rvcarvalho)                   */
-/*                      Leonardo Vogel Dalcin - 243654   (lvdalcin)                      */
-
-#include <stdio.h>
-#import "lex.yy.c"
-
-
-int main() {
-//    FILE* testFile = fopen("/Users/leonardodalcin/CLionProjects/compiler/testFile.txt", "r");
-    FILE* testFile = fopen("/Users/Rodolfo/Documents/Mega/UFRGS/Compiladores/compiler/testFile.txt", "r");
-  int token;
-  yyin = testFile;
-  while( (token = yylex()) ) {
-    printf("at line  %d \n", getLineNumber());
-
-  }
-  hashPrint();
-  return 0;
-}
-
-
+//
+// Created by Leonardo Dalcin on 3/18/18.
+//
 
