811c040421f56bb24b08abcd882fe379ca93dc76
Rodolfo Viola <rodolfoviolac@gmail.com>
2018-05-25 00:09:47 -0300

    Added char & float

--- semantic.c
+++ semantic.c
@@ -16,14 +16,20 @@ void set_declarations(ASTREE *node)
             exit(4);
         }
         node->son[1]->symbol->type = SYMBOL_INTEGER;
-        if(node->son[1]->symbol->type == KW_INT)
-        {
-            if(node->son[2]->type == LIT_INTEGER){
-                node->datatype = DATATYPE_INT;
-            }
+        if(node->son[1]->symbol->type == KW_INT) {
+            if(node->son[2]->type == LIT_INTEGER) node->datatype = DATATYPE_INT;
 
         }
+        if(node->son[1]->symbol->type == KW_FLOAT) {
+            if(node->son[2]->type == LIT_INTEGER) node->datatype = DATATYPE_INT;
+
+        }
+        if(node->son[1]->symbol->type == KW_CHAR) {
+            if(node->son[2]->type == LIT_INTEGER) node->datatype = DATATYPE_INT;
+        }
     }
+
+
 
   for (int i=0;i<MAX_SONS; ++i){
       set_declarations(node->son[i]);
